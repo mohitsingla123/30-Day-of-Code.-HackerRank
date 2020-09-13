e173ce297019b6a33a55a55938a1ef2ea27ebd68
Mohit Kumar <singla45567@gmail.com>
2020-09-13 11:02:07 +0530

    Create Day_06_Lets_Review.java

---
+++ Day_06_Lets_Review.java
@@ -0,0 +1,93 @@
+// Problem Statement Link: https://www.hackerrank.com/challenges/30-review-loop/problem
+//Solution with all test-cases passed.
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void review(String a)
+    {
+        int len = a.length();
+        for(int j =0;j<len;j++)
+        {
+            if(j%2==0)
+            {
+                System.out.print(a.charAt(j));
+            }
+        }
+
+        System.out.print(" ");
+        for(int j =0;j<len;j++)
+        {
+            if(j%2 != 0)
+            {
+                System.out.print(a.charAt(j));
+            }
+        }
+        System.out.println("");
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        for(int i =0;i<n;i++)
+        {
+        String a = scan.next();
+        review(a);
+        }
+    }
+} 
+
+
+
+/*
+
+Problem Statement: 
+Objective
+Today we're expanding our knowledge of Strings and combining it with what we've already learned about loops. Check out the Tutorial tab for learning materials and an instructional video!
+
+Task
+Given a string, , of length  that is indexed from  to , print its even-indexed and odd-indexed characters as  space-separated strings on a single line (see the Sample below for more detail).
+
+Note:  is considered to be an even index.
+
+Input Format
+
+The first line contains an integer,  (the number of test cases).
+Each line  of the  subsequent lines contain a String, .
+
+Constraints
+
+Output Format
+
+For each String  (where ), print 's even-indexed characters, followed by a space, followed by 's odd-indexed characters.
+
+Sample Input
+
+2
+Hacker
+Rank
+Sample Output
+
+Hce akr
+Rn ak
+Explanation
+
+Test Case 0: 
+
+
+
+
+
+
+The even indices are , , and , and the odd indices are , , and . We then print a single line of  space-separated strings; the first string contains the ordered characters from 's even indices (), and the second string contains the ordered characters from 's odd indices ().
+
+Test Case 1: 
+
+
+
+
+The even indices are  and , and the odd indices are  and . We then print a single line of  space-separated strings; the first string contains the ordered characters from 's even indices (), and the second string contains the ordered characters from 's odd indices ().
